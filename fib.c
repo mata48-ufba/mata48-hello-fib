f49dba739cb5026a418efad233792f25ac405e0f
Vinicius Petrucci <vpetrucci@users.noreply.github.com>
2021-03-03 03:48:55 -0500

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n) {
+
+/* code here */
+
+}
+
+int main(int argc, const char* argv[]) {
+    int n = atoi(argv[1]);
+    printf("fib(%d) = %d\n", n, fib(n));
+    return 0;
+}
